50021612ffbb984a649f7e54140db5f5745663ef
Gillianch <gillianc13@nycstudents.net>
2026-09-23 09:02:47 -0400

    luj

--- app.py
+++ app.py
@@ -1,7 +1,61 @@
-import turtle
-from turtle import *
-t = Turtle()
+# import turtle
+# from turtle import *
+# t = Turtle()
 
-t.shape('turtle')
+# t.shape('turtle')
 
-t.forward(200)
+# t.forward(200)
+
+# def message(input):
+#     print(input)
+# message("Hello Class")
+
+# def square(x):
+#     t.forward(x)
+#     t.left(90)
+#     t.forward(x)
+#     t.left(90)
+#     t.forward(x)
+#     t.left(90)
+#     t.forward(x)
+#     t.left(90)
+# square(200)
+
+# def equal(x):
+#     t.forward(x)
+#     t.left(120)
+#     t.forward(x)
+#     t.left(120)
+#     t.forward(x)
+# equal(200)
+
+# def right(x):
+#     t.forward(100)
+#     t.left(90)
+#     t.forward(100)
+#     t.left(135)
+#     t.forward(142)
+# right(200)
+
+# def rectangle(x):
+#     t.forward(100)
+#     t.left(90)
+#     t.forward(125)
+#     t.left(90)
+#     t.forward(100)
+#     t.left(90)
+#     t.forward(125)
+#     t.left(90)
+# rectangle(200)
+
+# def equal(x):
+#     t.forward(90)
+#     t.left(120)
+#     t.forward(90)
+#     t.left(120)
+#     t.forward(90)
+#     t.left(120)
+# equal(200)
+    
+    
+turtle.done()
